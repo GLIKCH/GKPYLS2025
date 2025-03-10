4034b5c3f025264f630594d2b408fd09c3b82a1f
Joel De Alba <sightlinepcs@outlook.com>
2025-03-10 18:41:56 -0400

    FTENCOM0003

---
+++ react_fren_dev_2025/src/pages/products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>This is the products page where we showcase the products we offer.</p>
+    </div>
+  );
+}
+
+export default Products;
---
+++ react_fren_dev_2025/src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>Learn more about the services we provide to our customers.</p>
+    </div>
+  );
+}
+
+export default Services;
